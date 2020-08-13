d5e6ac5f3a4359b1b8d01f140ccfc0409f08545e
Carlos Roberto Augusto Junior <carlosrbta@gmail.com>
2020-08-13 10:10:10 -0300

    Fixes on widget index page

--- src/pages/Widget/index.js
+++ src/pages/Widget/index.js
@@ -1,30 +1,89 @@
-import React from "react";
-import { Container, Navbar, Form, FormControl } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import Header from "../../components/Header";
 import WidgetCard from "../../components/WidgetCard";
+import ConfirmDialog from "../../components/ConfirmDialog";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { widgetsKey } from "../../constants";
+
+import "./index.css";
 
 function WidgetPage() {
-  const [widgets] = useLocalStorage("widgets", [
-    {
-      title: {
-        text: "",
-      },
-      series: [
-        {
-          data: [1, 2, 3, 3, 2, 4, 1],
-        },
-      ],
-    },
-  ]);
+  const [confirmDialog, setConfirmDialog] = useState(false);
+  const [search, setSearch] = useState("");
+  const [widgetId, setWidgetId] = useState(null);
 
-  console.log(widgets);
+  const [widgets, setWidgets] = useLocalStorage(widgetsKey);
+
+  const history = useHistory();
+
+  const onAddItem = () => history.push("/widget/form");
+
+  const onEditItem = (itemId) => history.push(`/widget/form/${itemId}`);
+
+  const onRemoveItem = (itemId) => {
+    setWidgetId(itemId);
+    setConfirmDialog(true);
+  };
+
+  const onClickClose = () => {
+    setWidgetId(null);
+    setConfirmDialog(false);
+  };
+
+  const onClickConfirm = () => {
+    setWidgets(widgets.filter((e) => e.id !== widgetId));
+    setWidgetId(null);
+    setConfirmDialog(false);
+  };
+
+  const handleSearchChange = (event) => setSearch(event.target.value);
+
+  const onFilter = (item) => {
+    if (search) {
+      return item.title.toLowerCase().includes(search);
+    }
+    return item;
+  };
+
+  const RenderNoResults = () =>
+    widgets.filter((e) => onFilter(e)).length == 0 && (
+      <div>No results for {search}.</div>
+    );
+
+  const RenderWidgets = () =>
+    widgets
+      .filter((e) => onFilter(e))
+      .sort((a, b) => a.title.localeCompare(b.title))
+      .map((item) => (
+        <WidgetCard
+          key={item.title}
+          widget={item}
+          onClickEdit={onEditItem}
+          onClickRemove={onRemoveItem}
+        />
+      ));
 
   return (
-    <Container>
-      {widgets.map((widget) => (
-        <WidgetCard key={widget.title} options={widget} />
-      ))}
-    </Container>
+    <>
+      <Header showSearch onSearch={handleSearchChange} />
+      <Container>
+        <RenderNoResults />
+        <RenderWidgets />
+
+        <Button className="btn-add" variant="secondary" onClick={onAddItem}>
+          +
+        </Button>
+        <ConfirmDialog
+          title="Widget remove"
+          description="Do really want to remove this widget?"
+          isVisible={confirmDialog}
+          onClickClose={onClickClose}
+          onClickConfirm={onClickConfirm}
+        />
+      </Container>
+    </>
   );
 }
 
