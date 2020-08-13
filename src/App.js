16d372a7f452b8ff5f118fbdf578645891e4484e
Carlos Roberto Augusto Junior <carlosrbta@gmail.com>
2020-08-13 10:10:27 -0300

    App js and package with libs

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
-import { Container, Navbar, Form, FormControl } from "react-bootstrap";
-
-import Widget from "./pages/Widget";
-import WidgetForm from "./pages/Widget/form";
-
-import logo from "./assets/logo.png";
-import "./App.css";
+import WidgetPage from "./pages/Widget";
+import WidgetFormPage from "./pages/WidgetForm";
 
 function App() {
   return (
     <Router>
-      <Navbar expand="lg" variant="light" bg="light">
-        <Container>
-          <Navbar.Brand href="#home" className="mr-auto">
-            <img src={logo} className="brand-logo" alt="Delfos Logo" />
-          </Navbar.Brand>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          </Form>
-        </Container>
-      </Navbar>
-
       <Switch>
         <Route exact path="/">
-          <Widget />
+          <Redirect to="/widget" />
         </Route>
-        <Route path="/Widget">
-          <WidgetForm />
+        <Route exact path="/widget">
+          <WidgetPage />
+        </Route>
+        <Route path="/widget/form/:id?">
+          <WidgetFormPage />
         </Route>
       </Switch>
     </Router>
