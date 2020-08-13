a8f06303009ea6d16cbfeb411670b41c69797e52
Carlos Roberto Augusto Junior <carlosrbta@gmail.com>
2020-08-13 10:10:02 -0300

    Add widget form page

---
+++ src/pages/WidgetForm/index.js
@@ -0,0 +1,151 @@
+import React, { useState, useEffect } from "react";
+import { Container, Form, Button, Col } from "react-bootstrap";
+import { maxBy } from "lodash";
+import { useParams, useHistory } from "react-router-dom";
+import Header from "../../components/Header";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { widgetsKey } from "../../constants";
+import "./index.css";
+
+function WidgetFormPage() {
+  const [formTitle, setTitle] = useState();
+  const [formSeries, setSeries] = useState();
+  const [errors, setErrors] = useState({});
+
+  const [widgets, setWidgets] = useLocalStorage(widgetsKey, []);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (id) {
+      const widget = widgets.find((e) => e.id === Number(id));
+
+      if (!widget) return;
+      setTitle(widget.title);
+      setSeries(widget.series.join(", "));
+    }
+  }, [id, widgets]);
+
+  const isValidForm = () => {
+    let valid = true;
+    let result = { ...errors };
+
+    if (!formTitle || formTitle === "") {
+      result = { ...result, title: true };
+      valid = false;
+    }
+
+    if (!formSeries || formSeries === "") {
+      result = { ...result, series: true };
+      valid = false;
+    }
+
+    setErrors(result);
+    return valid;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!isValidForm()) return;
+
+    const maxValue = maxBy(widgets, "id") || { id: 0 };
+
+    const result = formSeries
+      .replace(/\s/g, "")
+      .split(",")
+      .map((x) => parseInt(x, 10));
+
+    setWidgets([
+      ...widgets.filter((e) => e.id !== Number(id)),
+      {
+        id: Number(id) || maxValue.id + 1,
+        title: formTitle,
+        series: result,
+      },
+    ]);
+
+    history.push("/widget");
+  };
+
+  const onClickBack = () => history.push("/widget");
+
+  const handleChangeTitle = (event) => setTitle(event.target.value);
+
+  const handleChangeSeries = (event) => {
+    const value = event.target.value;
+    const digit = value.charAt(value.length - 1);
+    const last = value.charAt(value.length - 2);
+
+    // Event should be clear
+    if (digit === "" && last === "") {
+      setSeries("");
+      return;
+    }
+
+    // Event should de Number, Space or Comma
+    if (!Number(digit) && digit !== " " && digit !== ",") return;
+
+    // Event should be Number when last is Space
+    if (last === " " && !Number(digit)) return;
+
+    setSeries(event.target.value);
+  };
+
+  const pageTitle = id ? "Edit widget" : "Create widget";
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <p className="h4">{pageTitle}</p>
+        <Form onSubmit={handleSubmit}>
+          <Form.Row>
+            <Col lg={4} md={6}>
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                onChange={handleChangeTitle}
+                value={formTitle}
+              />
+
+              {errors.title && <Form.Text>Title is required.</Form.Text>}
+            </Col>
+          </Form.Row>
+
+          <Form.Row>
+            <Col lg={4}>
+              <Form.Label>Series</Form.Label>
+              <Form.Control
+                type="text"
+                onChange={handleChangeSeries}
+                value={formSeries}
+              />
+
+              {errors.series && <Form.Text>Series is required.</Form.Text>}
+
+              <Form.Text className="text-muted">
+                Should be separate with comma, eg: 5, 3, 4, 5, 7, 10
+              </Form.Text>
+            </Col>
+          </Form.Row>
+
+          <Form.Row>
+            <Col>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+
+              <Button variant="secondary" type="button" onClick={onClickBack}>
+                Back
+              </Button>
+            </Col>
+          </Form.Row>
+        </Form>
+      </Container>
+    </>
+  );
+}
+
+export default WidgetFormPage;
